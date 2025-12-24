6cee670f56730bbc0a1f64fd045e29e5d3df5cb7
Jaber Ahmed <jabarahmed332@gmail.com>
2025-12-24 19:59:37 +0600

    Add Entry Experience: Empty screen with animated object, scroll/click to enter main experience

--- ecommerce_website/src/App.tsx
+++ ecommerce_website/src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import Entry from './components/Entry';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import BrandStory from './components/BrandStory';
@@ -6,6 +8,12 @@ import Cart from './components/Cart';
 import Footer from './components/Footer';
 
 export default function App() {
+  const [hasEntered, setHasEntered] = useState(false);
+
+  if (!hasEntered) {
+    return <Entry onEnter={() => setHasEntered(true)} />;
+  }
+
   return (
     <div className="App">
       <Navbar />
